59117020f4f75ac2123109de015b201335b6c2bb
Olasubomi Awokoya <olasubomi.awokoya@hotmail.com>
2018-07-04 07:51:55 -0700

    Modifying directly to deploy files to heroku using nodejs

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public_html')))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
