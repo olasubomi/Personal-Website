18650a24771477b07f07883617478207dbbaf5de
Olasubomi Awokoya <olasubomi.awokoya@hotmail.com>
2018-07-13 10:24:38 -0700

    adding test statement

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const config = {
     port: 5432
 };
 
+
 express()
   .use(express.static(path.join(__dirname, 'public_html')))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => console.log('Hello World!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
